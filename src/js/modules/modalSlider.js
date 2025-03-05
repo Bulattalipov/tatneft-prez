e81551c3f0e2234d68807711669628c679d7c2db
Талипов Булат Альфатович <BTalipov@tisbi.ru>
2025-03-05 16:46:14 +0300

    added video

--- src/js/modules/modalSlider.js
+++ src/js/modules/modalSlider.js
@@ -6,17 +6,20 @@ Swiper.use([Navigation]);
 
 export default function () {
 
-  const slider = document.querySelector('.modal__slider');
+  const sliders = document.querySelectorAll('.modal__slider');
 
-  new Swiper(slider, {
-    // slidesPerView: "auto",
-    centeredSlides: true,
-    speed: 500,
-    spaceBetween: 30,
-    // autoHeight: true,
-    navigation: {
-      nextEl: slider.querySelector('.swiper-nav__btn-next'),
-      prevEl: slider.querySelector('.swiper-nav__btn-prev')
-    },
-  });
+  sliders.forEach(slider => {
+    new Swiper(slider, {
+      // slidesPerView: "auto",
+      centeredSlides: true,
+      speed: 500,
+      spaceBetween: 30,
+      // autoHeight: true,
+      navigation: {
+        nextEl: slider.querySelector('.swiper-nav__btn-next'),
+        prevEl: slider.querySelector('.swiper-nav__btn-prev')
+      },
+    });
+  })
+
 }
