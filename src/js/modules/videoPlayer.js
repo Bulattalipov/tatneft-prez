9481c70db36fcfe760c82a8c675c9d0f607864cd
Талипов Булат Альфатович <BTalipov@tisbi.ru>
2025-03-06 13:19:23 +0300

    update

--- src/js/modules/videoPlayer.js
+++ src/js/modules/videoPlayer.js
@@ -6,4 +6,18 @@ export default function() {
   //     autoplay: false,
   //     preload: 'auto'
   // });
+
+  const videoBlocks = document.querySelectorAll('.modal__slider-item-video');
+
+  if(videoBlocks.length === 0) return;
+
+  videoBlocks.forEach(videoBlock => {
+    const videoElem = videoBlock.querySelector('.modal__slider-item-video-elem');
+    const btn = videoBlock.querySelector('.modal__slider-item-video-play');
+    btn.addEventListener('click', function() {
+      this.style.display = 'none';
+      videoElem.setAttribute('controls', true);
+      videoElem.play();
+    });
+  })
 }
